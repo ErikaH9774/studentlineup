49ca2be4c8fac637c63f513dad07dff7afdbec4d
ErikaH92 <erv27240@email.vccs.edu>
2025-05-09 23:54:22 -0400

    added comment

--- studentlineup/studentlineup.cpp
+++ studentlineup/studentlineup.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main()
 {
-
+	//where the file is being pulled out when using this format
 	ifstream inputFile("LineUp.txt");
 	string name;
 	string firstName, Lastname;
@@ -17,25 +17,26 @@ int main()
 	int count = 0;
 	//check if the file opens correctly 
 	if (!inputFile) {
-
+		//will receive error pop up if the file cannot open. 
 		cerr << "Error: Cannot open file LineUp.txt" << endl;
 		return 1;
 	}
 	//Read the first name to iniliaziles comparison
-
+	//if statement if the file names is greater than name then proceed with the next statement
 	if (inputFile >> name) {
 
 		firstName = name;
 		Lastname = name;
 		count = 1;
 	}
+	//otherwise it will display no names found in the file
 	else {
 
 		cout << "No names found in the file" << endl;
 
 		return 0;
-
-	} while (inputFiles >> name) {
+		//while input file is greater than name then first name is equal to name and name is greater than lastname
+	} while (inputFile >> name) {
 
 		firstName = name;
 	}
@@ -44,11 +45,11 @@ int main()
 		Lastname = name;
 	}
 
-} inputFiles.close() {
-
-	cout << " Number of students: \n" << count << "." endl;
-	cout << " First in line: " << firstName << "." endl;
-	cout << "Last in line: " << Lastname << "." endl;
+ inputFile.close(); 
+ //displays number of students first in line and last in line. 
+ cout << " Number of students: \n" << count << "."; 
+ cout << " First in line: " << firstName << "."; 
+ cout << "Last in line: " << Lastname << "."; 
 
 	return 0;
 }
